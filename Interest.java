8ee97ab47825bf4bdcc030398e28a48d7083ab1f
Kameron Ibraheem <kameronibraheem@gmail.com>
2024-12-02 19:29:11 -0600

    Create Interest.java

---
+++ Interest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author kamer
+ */
+// Interest.java
+// Kameron Ibraheem
+// Lab 08: Interest
+//
+//
+
+public interface Interest {
+    double FIX_RATE = 2.5;
+
+    double annualInterest();
+}
+
